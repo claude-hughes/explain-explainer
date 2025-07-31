4ab0363d724db35fe369c3d1f586b736dc1025e3
Doug Hughes <doug@doughughes.net>
2025-07-31 10:43:29 -0400

    nuke everything

--- src/translator/index.ts
+++
@@ -1,8 +0,0 @@
-export { PostgreSQLTranslator, translatePostgreSQLPlan } from './translator';
-export { 
-  getNodeExplanation, 
-  detectPerformanceIssues, 
-  generateRecommendations,
-  type NodeExplanation,
-  type ExecutionStep
-} from './explanations';
